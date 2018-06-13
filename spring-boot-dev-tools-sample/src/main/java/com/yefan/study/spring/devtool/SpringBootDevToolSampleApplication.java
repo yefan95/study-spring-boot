24a239122f3568e2271233a5e535a17cb5d9c940
yefan <yefan9512@gmail.com>
2018-06-13 21:14:20 +0800

    commit dev-tool sample

---
+++ spring-boot-dev-tools-sample/src/main/java/com/yefan/study/spring/devtool/SpringBootDevToolSampleApplication.java
@@ -0,0 +1,12 @@
+package com.yefan.study.spring.devtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevToolSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevToolSampleApplication.class, args);
+	}
+}
